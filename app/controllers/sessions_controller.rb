6096ca997308be45e22b2ac77515bc5f1a5772ee
Dan Comand <dcomand@gmail.com>
2015-06-17 14:35:12 -0400

    Added downcase to email params so that logging in is better UX.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,7 +3,7 @@ class SessionsController < ApplicationController
   end
 
    def create
-    user = User.find_by(email: params[:email])
+    user = User.find_by(email: params[:email].downcase)
     if user && user.authenticate(params[:password])
       session[:user_id] = user.id
       redirect_to lists_url, notice: "Logged in!"
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ActiveRecord::Base
-  has_secure_password
-  has_many :bookmarks
+	validates :email, :case_sensitive => false 
+	has_secure_password
+	has_many :bookmarks
 end
